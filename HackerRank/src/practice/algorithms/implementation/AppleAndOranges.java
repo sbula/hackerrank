b56cf797b526dcd14d2828ace41bc75e1f451dce
sbula <steve.bual@gmail.com>
2017-04-02 18:59:00 +0200

    solved challenge

---
+++ HackerRank/src/practice/algorithms/implementation/AppleAndOranges.java
@@ -0,0 +1,23 @@
+package practice.algorithms.implementation;
+
+import java.util.Scanner;
+
+public class AppleAndOranges {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int t = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int[] apple = new int[m];
+        for(int apple_i=0; apple_i < m; apple_i++){
+            apple[apple_i] = in.nextInt();
+        }
+        int[] orange = new int[n];
+        for(int orange_i=0; orange_i < n; orange_i++){
+            orange[orange_i] = in.nextInt();
+        }
+    }
+}
