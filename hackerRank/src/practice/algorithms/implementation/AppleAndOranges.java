7f631cbe4051648f57fa972afbaf8506bdf29261
sbula <steve.bual@gmail.com>
2017-04-03 09:34:44 +0200

    new challenge solved (reverted from commit 09a7eb4170cf4fef9b94943686e185f21181614d)

--- hackerRank/src/practice/algorithms/implementation/AppleAndOranges.java
+++ hackerRank/src/practice/algorithms/implementation/AppleAndOranges.java
@@ -3,34 +3,21 @@ package practice.algorithms.implementation;
 import java.util.Scanner;
 
 public class AppleAndOranges {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int houseL = in.nextInt();
-        int houseR = in.nextInt();
-        int appleTree = in.nextInt();
-        int orangeTree = in.nextInt();
+        int s = in.nextInt();
+        int t = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
         int m = in.nextInt();
         int n = in.nextInt();
-        
-        int apples = 0;
+        int[] apple = new int[m];
         for(int apple_i=0; apple_i < m; apple_i++){
-        	int fallingApple = appleTree + in.nextInt();
-            if(houseL <= fallingApple && fallingApple <= houseR) {
-            	apples++;
-            }
+            apple[apple_i] = in.nextInt();
         }
-
-        int oranges = 0;
+        int[] orange = new int[n];
         for(int orange_i=0; orange_i < n; orange_i++){
-        	int fallingOranges = orangeTree + in.nextInt();
-            if(houseL <= fallingOranges && fallingOranges <= houseR) {
-            	oranges++;
-            }
+            orange[orange_i] = in.nextInt();
         }
-        in.close();
-
-        System.out.println(apples);
-        System.out.println(oranges);
-	}
+    }
 }
